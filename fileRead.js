0c22c6beece6b3988bb81854c448831fbc42a941
Abhishek <abhikharpal@gmail.com>
2023-01-19 12:30:08 +0530

    feat: add functions to read file in synchronous manner using Promise

---
+++ fileRead.js
@@ -0,0 +1,47 @@
+const fs =require('fs');
+
+let customFileReader = (fileName) =>{
+
+  return new Promise((resolve,reject) => {
+    fs.readFile('textFiles/'+`${fileName}`,'utf-8',(err,text)=>{
+      if(err){
+        reject(err);
+      }
+      else{
+        resolve(text);
+      }
+    });
+  });
+};
+
+
+let filesReader = ()=>{
+
+  let files =['A.txt','B.txt','C.txt'];
+
+  for(let i=0;i<3;i++){
+    fs.readFile('textFiles/'+`${files[i]}`,'utf-8',(err,text)=>{
+      if(err){
+        throw err;
+      }
+      console.log(text.toString());
+    });
+  }
+
+  for(let i=0;i<3;i++){
+    console.log('Now in sync');
+    let data = fs.readFileSync('textFiles/'+`${files[i]}`,{encoding:'utf-8',flag: 'r'});
+    console.log(data);
+  }
+
+};
+
+customFileReader('A.txt').then(val=>{
+  console.log(val);
+  customFileReader('B.txt').then(val=>{
+    console.log(val);
+    customFileReader('C.txt').then(val=>{
+      console.log(val);
+    });
+  });
+});
